cf6ab40f1a99b2570483cce402e6dfb03bd9c50f
Nikhilesh <gvvranbnikhilesh@gmail.com>
2026-06-11 01:18:52 +0530

    Auto-create superuser in build script

--- build.sh
+++ build.sh
@@ -5,3 +5,9 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Automatically create a superuser if environment variables are provided
+if [[ -n "${DJANGO_SUPERUSER_USERNAME}" && -n "${DJANGO_SUPERUSER_PASSWORD}" ]]; then
+  echo "Creating superuser..."
+  python manage.py createsuperuser --noinput || echo "Superuser already exists."
+fi
